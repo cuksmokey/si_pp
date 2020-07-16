350be8f3e990b2f5a4ba689b92033545498c7008
cuksmokey <bolongs360@gmail.com>
2020-07-16 22:06:31 +0700

    edit m_login

--- application/models/m_login.php
+++ application/models/m_login.php
@@ -4,6 +4,7 @@ class M_login extends CI_Model{
 
 	function cek_login($username,$password){
 		// $this->load->database('default', TRUE);
+		// $this->load->database('default', TRUE);
 		$query = "SELECT * FROM user WHERE username = '".$username."' AND password = '".$password."'";
 		return $this->db->query($query);
 
