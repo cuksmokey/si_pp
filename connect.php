af5e6fc2a7de3e9c055ab9835dceb0f235ae9e97
cuksmokey <bolongs360@gmail.com>
2020-07-16 15:43:06 +0700

    sssssss

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 
-// $conn = mysql_connect("localhost", "root", "") or die(mysql_error());
-// mysql_select_db("db_sipp", $conn) or die(mysql_error());
-
-$conn = mysql_connect("localhost", "sinarmukti", "sinarmukti") or die(mysql_error());
+$conn = mysql_connect("localhost", "root", "") or die(mysql_error());
 mysql_select_db("db_sipp", $conn) or die(mysql_error());
+
+// $conn = mysql_connect("localhost", "sinarmukti", "sinarmukti") or die(mysql_error());
+// mysql_select_db("db_sipp", $conn) or die(mysql_error());
 
 ?>
