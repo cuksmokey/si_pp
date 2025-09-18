bdafa2ff49ff86f6ed723c37498a0adf701f7421
cuksmokey <bolongs360@gmail.com>
2025-09-18 23:59:32 +0700

    dbdb

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,9 +1,9 @@
 <?php
 
-class Login extends CI_Controller{
+class login extends CI_Controller{
 
 	function __construct(){
-		parent::__construct();		
+		parent::__construct();	
 		$this->load->model('M_login');
 		$this->load->model('M_master');
 
